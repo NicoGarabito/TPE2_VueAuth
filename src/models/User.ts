4c23a0d8e43273024c6de5781c16d6625aba6596
NicoGarabito <146384542+NicoGarabito@users.noreply.github.com>
2024-10-14 19:03:01 -0300

    cambios de video 1 aplicados

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,11 @@
 export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
   user: string;
   password: string;
-  remember: boolean;
+  remember?: boolean;
+  isAdmin: boolean;
+  jwtToken?: string;
+  refreshTokens: string[];
 }
