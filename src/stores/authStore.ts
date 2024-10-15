a40b3eea508a9035ddb45902c7129f239e08823f
NicoGarabito <146384542+NicoGarabito@users.noreply.github.com>
2024-10-14 21:49:53 -0300

    Cambios de video 4 ;)

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,6 +1,7 @@
 import {defineStore} from 'pinia'
 import { fetchWrapper } from '@/helpers/fetchwrapper'
 import type {User} from '@/models/User'
+import router from '@/router'
 
 
 const baseUrl = `${import.meta.env.VITE_API_URL}/users`
@@ -12,10 +13,38 @@ export const useAuthStore = defineStore({
     }),
 
     actions: {
+        async login(user: string, password: string) {
+            this.auth.data = await fetchWrapper.post('${baseUrl}/authenticate', {user, password},{ credentials: 'include' });
+            this.starRefreshTokenTimer();
+        },
         logout() {
             fetchWrapper.post('${baseUrl}/revoke-token', { credentials: 'include' });
-        }
+            this.stopRefreshTokenTimer();
+            this.auth.data = null;
+            router.push({name: '/login'});
+        },
 
-    }
-    
+        async refreshToken() {
+            this.auth.data = await fetchWrapper.post('${baseUrl}/refresh-token',{},{ credentials: 'include' });
+            this.starRefreshTokenTimer();
+        },
+        starRefreshTokenTimer() {
+            if(!this.auth.data || !this.auth.data.jwtToken) return;
+
+            const jwtBase64 = this.auth.data.jwtToken.split('.')[1];
+            const decodedJwtToken = JSON.parse(atob(jwtBase64[1]))
+
+            const expires = new Date(decodedJwtToken.exp * 1000);
+            const timeout = expires.getTime() - Date.now() - (60 * 1000);
+
+            this.auth.refreshTokenTimeout = setTimeout(this.refreshToken, timeout);
+        },
+
+        stopRefreshTokenTimer() {
+            if(this.auth.refreshTokenTimeout) {
+                clearTimeout(this.auth.refreshTokenTimeout);
+                this.auth.refreshTokenTimeout = null;
+            }
+        }
+    }    
 })
